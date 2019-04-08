e26731472e41590f655539b26819e41390517d24
Janek Pachnik <januzpachnik@gmailcom>
2019-04-08 12:10:09 +0200

    data manageer questions

--- connection.py
+++ connection.py
@@ -1,14 +1,14 @@
 import csv
 
 
-def import_question(filename):  # import csv data
+def import_data(filename):  # import csv data
     with open(filename, "r") as file:
         imported_data = csv.DictReader(file)
         data = list(imported_data)
         return data
 
 
-def write_file(dict_list, filename="questions.csv"):
+def write_file(dict_list, filename):
     keys = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
     if filename == 'answers.csv':
         keys = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
--- server.py
+++ server.py
@@ -14,12 +14,14 @@ def main_page():
 def list_of_questions():
     return render_template("list.html")
 
+@app.route('/list/show_question')       #transfers id from list of questions
+def show_question:
+    return render_template(show_question)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_question():
     question = request.form
     return render_template("add.html")
-
 
 @app.route('/about')
 def about():

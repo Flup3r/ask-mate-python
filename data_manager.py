41a4bad6ef24aa5319fd28166493d53b802e07ad
Janek Pachnik <januzpachnik@gmailcom>
2019-04-08 15:03:37 +0200

    pojedyczę pytania

--- data_manager.py
+++ data_manager.py
@@ -11,9 +11,17 @@ import connection
 
 
 def get_all_answers():
-    a_list = connection.import_data('answers.csv')
+    a_list = connection.import_data('ask-mate-python/sample_data/answers.csv')
     return a_list
 
+
 def get_questions():
-    q_list = connection.import_data('./sample_data/question.csv')
+    q_list = connection.import_data('ask-mate-python/sample_data/question.csv')
     return q_list
+
+
+def one_question(id):
+    questions = get_questions()
+    id = int(id)
+    question = questions[id]
+    return question

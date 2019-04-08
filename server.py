212a513610543f7a3a23ddba9c887aa727495eae
Janek Pachnik <januzpachnik@gmailcom>
2019-04-08 11:34:38 +0200

    c

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 import data_manager
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    question = request.form
+    return render_template("index.html")
+
+
+@app.route('/list')
+def list_of_questions():
+    return render_template("list.html")
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_question():
+    question = request.form
+    return render_template("add.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 
 if __name__ == '__main__':
